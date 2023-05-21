Fixed wrong height calculation, removed png

inverse.py
@@ -10,9 +10,17 @@ mesh_path = 'mesh_16.dat'
 
 # mesh = get_mesh(mesh_path)
 receivers = get_receivers(receivers_results_path)
-start_pnt = Point(-500, 0, 0)
-end_pnt = Point(400, 100, -300)
+start_pnt = Point(-500, -50, 0)
+end_pnt = Point(400, 50, -300)
 mesh = generate_mesh(start_pnt, end_pnt, 9, 1, 6)
+
+# start_pnt = Point(-100, -50, -100)
+# end_pnt = Point(100, 50, -200)
+# mesh = generate_mesh(start_pnt, end_pnt, 2, 1, 2)
+
+# start_pnt = Point(-200, -50, -50)
+# end_pnt = Point(200, 50, -250)
+# mesh = generate_mesh(start_pnt, end_pnt, 4, 1, 4)
 
 L = np.zeros(shape=(len(receivers) * 3, len(mesh) * 3))
 # L = np.zeros(shape=(len(receivers), len(mesh) * 3))
@@ -47,8 +55,8 @@ b = np.matmul(L.transpose(), S)
 # print(f"A = {A}")
 # print(f"b = {b}")
 
-alfa = 10**-5
-# alfa = 0
+# alfa = 10**-5
+alfa = 0
 ones = np.eye(len(A))
 regularizedA = A + np.dot(alfa, ones)
 p = np.linalg.solve(regularizedA, b)