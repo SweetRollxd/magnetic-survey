59481f9563d1547a2ce5594653680cdf4248456f
SweetRollxd <sukharik@ngs.ru>
2023-12-14 02:46:24 +0700

    Added dataset generation

--- dataset_generator.py
+++ dataset_generator.py
@@ -3,7 +3,7 @@ import random
 from extras.generator import generate_mesh, generate_receivers
 from extras.cell import Cell
 from extras.point import Point
-from extras.custom_functions import write_mesh_to_file, calculate_receivers
+from extras.custom_functions import write_mesh_to_file, calculate_receivers, write_receivers_to_file
 from extras.receiver import Receiver
 
 
@@ -66,16 +66,21 @@ if __name__ == "__main__":
     # random.seed(102)
     areas = generate_areas(Nx, Nz)
     [print(area) for area in areas]
-    anomalous_area = areas[random.randint(0, len(areas)-1)]
-    print(f"Anomalous area = {anomalous_area}")
-    for cell in mesh:
-        if anomalous_area.contains(cell):
-            cell.p = (1, 0, 0)
+    dataset_size = 100
 
-    [print(cell) for cell in mesh]
-    write_mesh_to_file("./meshes/test_model.mes", mesh)
+    for i in range(0, 100):
+        anomalous_area = areas[random.randint(0, len(areas)-1)]
+        print(f"Anomalous area = {anomalous_area}")
+        for cell in mesh:
+            if anomalous_area.contains(cell):
+                cell.p = (1, 0, 0)
 
-    # receivers = generate_receivers(start_pnt.x, end_pnt.x, 40)
-    # calculate_receivers(mesh, receivers)
-    #
-    # print(receivers)
+        [print(cell) for cell in mesh]
+        dataset_path = "./datasets/dataset_0"
+        write_mesh_to_file(f"{dataset_path}/mesh_{i}.mes", mesh)
+
+        receivers = generate_receivers(start_pnt.x, end_pnt.x, 40)
+        calculate_receivers(mesh, receivers)
+        #
+        [print(recv) for recv in receivers]
+        write_receivers_to_file(f"{dataset_path}/receivers_{i}", receivers)
